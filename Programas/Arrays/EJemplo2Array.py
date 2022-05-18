a35d51cde6cd7e996502c7475967ef59d0a91640
nachoavi <luis17.sanmartin@gmail.com>
2022-05-17 22:23:16 -0400

    Se agregan ejemplos de Array

---
+++ Programas/Arrays/EJemplo2Array.py
@@ -0,0 +1,2 @@
+number_list = list((1,2,3,4))
+print(number_list)
---
+++ Programas/Arrays/Ejemplo3Array.py
@@ -0,0 +1,4 @@
+#Este es un ejemplo de array o lista y le agregamos el parametro range
+
+number_list = list(range(1,11))
+print(number_list)
