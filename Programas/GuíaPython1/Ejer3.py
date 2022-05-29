188dd0bd467380f8c5550ed300d18ca7898a9d8e
nachoavi <luis17.sanmartin@gmail.com>
2022-05-29 17:53:32 -0400

    Se añaden comentarios

--- Programas/GuíaPython1/Ejer3.py
+++ Programas/GuíaPython1/Ejer3.py
@@ -1,7 +1,7 @@
 #Crear un programa. permita ingresar la venta anual de un negocio (1 por cada mes), estos deben ser ingresados por teclado. Después imprimir por el promedio anual. 
 s=0
 for x in range (1,13): 
-    vta=int(input(F"Ingrese venta {x}:"))
+    vta=int(input(F"Ingrese venta {x}:")) #La "F" nos permite dar formato a la variable x para introducirlo a la str
     s=s+vta
 prom=s/12
 print("El promedio de las ventas es : ",prom)
