0c45e268dd2e670d3863005593e4688e8ce57e83
LUIS IGNACIO SAN MARTIN SANTANA <luis.sanmartin04@inacapmail.cl>
2022-06-02 15:18:10 -0400

    Se agregan ejercicios de la guía 2

---
+++ Programas/GuíaPython2/Ejer2.py
@@ -0,0 +1,6 @@
+n1=int(input("Ingrese un número: "))
+
+if n1%2==0:
+    print(n1, (" Es un numero par"))
+else:
+    print(n1, (" No es un numero par"))
--- Programas/GuíaPython2/Ejer3.py
+++ Programas/GuíaPython2/Ejer3.py
@@ -1,18 +1,13 @@
-#Realice un ALGORITMO que permita el Ingreso de 3 notas académicas.
-#- Validar notas entre 1,0 y 7,0, en caso contrario emitir mensaje de error
-#“Fuera de rango permitido” y volver a ingresar. (use un ciclos)
-#- Mostar promedio final de notas.
+from re import S
+
 
 x=1
-sum=0
-while x <=3:
-    n=float(input(f"Ingrese la nota {x}: " ))
-    if n >=1 and n <=7:
-        sum=sum+n
+while x==1:
+    n=int(input("Ingrese un numero: "))
+    n*=4
+    n/=2
+    print(n)
+    x=x-1
+    re=input("Desea ingresar un nuevo numero?[S/N]: ")
+    if re =="S":
         x=x+1
-        print("\nNota ingresada con exito\n")
-    else:
-        print("Nota fuera de rango, por favor vuelva a ingresar: ")
-        x-1
-p=sum/3
-print("El promedio es: ",p)
