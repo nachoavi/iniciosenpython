8ae3caa244b184195ba84dc6080d9a4876996334
Nacho <luis17.sanmartin@gmail.comgit config --global user.name nachoavigit config --global user.name nachoavi>
2022-06-10 00:19:30 -0400

    Se corrigen y modifican ejercicios

--- Programas/GuíaPython2/Ejer4.py
+++ Programas/GuíaPython2/Ejer4.py
@@ -5,44 +5,39 @@
 #pantalla que muestre el nombre de la persona y
 #su estado civil ej: “Ana es casada”. Al final
 #preguntar si desea ingresar un nuevo cliente.
-
+A="Soltero"
+B="Casado"
+C="Viudo"
+D="Divorciado"
 
 op="s"
 while(op=="s"):
     nom=input("Ingrese nombre: ")
     fon=input("Ingrese fono:")
-    print("Ingrese estado Civil: soltero/a, casado/a, viudo/a o divorciado/a:")
+    print("Selecione el estdo civil: \n[A]soltero/a \n [B]casado/a \n [C]viudo/a \n [D]divorciado/a:")
     x=1
     while (x==1):
         ec=input("Ingrese Estado Civil: ")
-        if(ec=="soltero" or ec=="soltera"):
-            print(nom, " es " , ec)
+        if(ec=="A" or ec=="a"):
+            print(nom, " es " , A)
             x=2
-        elif(ec=="casado" or ec=="casada"):
-            print(nom, " es " , ec)
+        elif(ec=="B" or ec=="b"):
+            print(nom, " es " , B)
             x=2
-        elif(ec=="viudo" or ec=="viuda"):
-            print(nom, " es " , ec)
+        elif(ec=="C" or ec=="c"):
+            print(nom, " es " , C)
             x=2
-        elif(ec=="divorciado" or ec=="divorciada"):
-            print(nom, " es " , ec)
+        elif(ec=="D" or ec=="d"):
+            print(nom, " es " , D)
             x=2
         else:
             print("ha ingresado una opción incorrecta, ingrese estado civil nuevamente")
 
     op=input("Desea ingresar otro cliente (s/n): ")
+    
     while(op!="n" and op!="s" and op!="N" and op!="S"):
         print("Ingrese una opción correcta, vuelva a intentar!!")
         op=input("Desea ingresar otro cliente (s/n): ")
 
 
 print("Fin del Programa")
-        
-        
-                
-
-
-            
-            
-            
-            
