d2a16907c61f15e192018d44a1cf288b18ae4e2a
Nacho <luis17.sanmartin@gmail.comgit config --global user.name nachoavigit config --global user.name nachoavi>
2022-07-06 16:02:18 -0400

    Pequeños cambios

---
+++ Programas/Ejercicios inacap/Prueba3/P5.py
@@ -0,0 +1,6 @@
+def ingresarTemp():
+    dia=["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"]
+    for dia in dia:
+        temp=float(input("Ingrese la temperatura del dia"+ dia))
+
+ingresarTemp()
