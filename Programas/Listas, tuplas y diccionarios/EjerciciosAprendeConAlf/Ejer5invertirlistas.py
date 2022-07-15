afae51a57a69de38cf5841470a7066a8f809014b
Nacho <luis17.sanmartin@gmail.comgit config --global user.name nachoavigit config --global user.name nachoavi>
2022-07-15 00:23:21 -0400

    Pequeños cambios

--- Programas/Listas, tuplas y diccionarios/EjerciciosAprendeConAlf/Ejer5invertirlistas.py
+++ Programas/Listas, tuplas y diccionarios/EjerciciosAprendeConAlf/Ejer5invertirlistas.py
@@ -1,4 +1,5 @@
+
 lista=[1,2,3,4,5,6,7,8,9,10]
-for i in range(9,-1,-1):
-    print(lista[i],end=", ")
+for i in range(1,11):
+    print(lista[-i],end=", ")
     
--- Programas/Listas, tuplas y diccionarios/GuíaListas/Ejer3.py
+++ Programas/Listas, tuplas y diccionarios/GuíaListas/Ejer3.py
@@ -9,7 +9,5 @@ print("\nLlenado lista 2")
 for x in range(0,5):
     num1.append(input(f"Ingrese el numero {x+1}: "))
 
-for x in range(0,5):
-    resul.append(num1+num2)
-print(resul)
+
 
